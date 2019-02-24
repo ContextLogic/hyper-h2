d2c07e44ec5c0df1ac4760025360e245a2ce9acc
Andrew Huang <k.andrew.huang@gmail.com>
2019-02-24 13:02:28 -0800

    Duplicate Setting for compatibility with hyper

--- h2/settings.py
+++ h2/settings.py
@@ -21,6 +21,35 @@ except ImportError:  # pragma: no cover
     # Python 2.7 compatibility
     from collections import MutableMapping
 
+#: Allows the sender to inform the remote endpoint of the maximum size of
+#: the header compression table used to decode header blocks, in octets.
+HEADER_TABLE_SIZE = SettingsFrame.HEADER_TABLE_SIZE
+
+#: This setting can be used to disable server push. To disable server push
+#: on a client, set this to 0.
+ENABLE_PUSH = SettingsFrame.ENABLE_PUSH
+
+#: Indicates the maximum number of concurrent streams that the sender will
+#: allow.
+MAX_CONCURRENT_STREAMS = SettingsFrame.MAX_CONCURRENT_STREAMS
+
+#: Indicates the sender's initial window size (in octets) for stream-level
+#: flow control.
+INITIAL_WINDOW_SIZE = SettingsFrame.INITIAL_WINDOW_SIZE
+
+#: Indicates the size of the largest frame payload that the sender is
+#: willing to receive, in octets.
+MAX_FRAME_SIZE = SettingsFrame.MAX_FRAME_SIZE
+
+#: This advisory setting informs a peer of the maximum size of header list
+#: that the sender is prepared to accept, in octets.  The value is based on
+#: the uncompressed size of header fields, including the length of the name
+#: and value in octets plus an overhead of 32 octets for each header field.
+MAX_HEADER_LIST_SIZE = SettingsFrame.MAX_HEADER_LIST_SIZE
+
+#: This setting can be used to enable the connect protocol. To enable on a
+#: client set this to 1.
+ENABLE_CONNECT_PROTOCOL = SettingsFrame.ENABLE_CONNECT_PROTOCOL
 
 class SettingCodes(enum.IntEnum):
     """
